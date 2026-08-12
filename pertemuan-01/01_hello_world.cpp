cd8c1b72df370e8ef9b343bbfe7ae67a03cb9ce1
illhammf <illhammf@users.noreply.github.com>
2026-08-12 09:44:29 +0700

    update 01_hello_world.cpp

---
+++ pertemuan-01/01_hello_world.cpp
@@ -0,0 +1,8 @@
+#include <iostream> // Untuk menggunakan library input/output stream
+
+int main() // Fungsi utama yang menjadi titik awal eksekusi program
+{
+    std::cout << "Hello World!"; // Menampilkan teks "Hello World!" ke layar menggunakan objek cout dari library iostream
+
+    return 0; // Mengembalikan nilai 0 ke sistem operasi
+}
