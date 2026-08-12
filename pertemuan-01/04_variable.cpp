c161eaf9048bba03f8b68d27e1b0e48027687c1b
illhammf <illhammf@users.noreply.github.com>
2026-08-12 09:54:51 +0700

    update 04_variable.cpp

---
+++ pertemuan-01/04_variable.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    int umur = 20; // Mendeklarasikan variabel umur dengan tipe data integer dan menginisialisasinya dengan nilai 20
+
+    std::cout << "Umur saya: " << umur << std::endl;
+
+    umur = 21; // Mengubah nilai variabel umur menjadi 21
+
+    std::cout << "Umur saya sekarang: " << umur << std::endl;
+
+    return 0;
+}
