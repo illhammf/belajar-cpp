76d8ceafd6bd93a9c8bb771ed2cc2c3bdbb64dd8
illhammf <illhammf@users.noreply.github.com>
2026-08-12 09:46:17 +0700

    update 02_output.cpp

---
+++ pertemuan-01/02_output.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Nama     : Ilham" << std::endl;
+    std::cout << "Bahasa   : C++" << std::endl;
+    std::cout << "Level    : Beginner" << std::endl;
+
+    return 0;
+}
