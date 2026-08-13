d0eeed83f606c4a5d848fe44f1e26e85938c8389
illhammf <illhammf@users.noreply.github.com>
2026-08-13 21:09:34 +0700

    update 06_const.cpp

---
+++ pertemuan-02/06_const.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const double PI = 3.14159; // Deklarasi konstanta PI dengan tipe data double, ini tidak dapat diubah nilainya setelah dideklarasikan
+
+    cout << "Nilai PI: " << PI << endl;
+
+    return 0;
+}
