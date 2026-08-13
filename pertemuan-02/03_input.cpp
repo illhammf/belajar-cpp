0cdbad86d082afdc442e0d134ecc7a64507e6a56
illhammf <illhammf@users.noreply.github.com>
2026-08-13 20:59:44 +0700

    update 03_input.cpp

---
+++ pertemuan-02/03_input.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int umur;
+
+    cout << "Masukkan umur: ";
+    cin >> umur;
+
+    cout << "Umur kamu: " << umur << endl;
+
+    return 0;
+}
