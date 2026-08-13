67041cc0074244efc9a1a5b282ada8c1547d1df3
illhammf <illhammf@users.noreply.github.com>
2026-08-13 21:10:50 +0700

    update 07_operator.cpp

---
+++ pertemuan-02/07_operator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10; // deklarasi dan inisialisasi variabel a dengan tipe data integer
+    int b = 3; // deklarasi dan inisialisasi variabel b dengan tipe data integer
+
+    cout << "Penjumlahan : " << a + b << endl;
+    cout << "Pengurangan : " << a - b << endl;
+    cout << "Perkalian   : " << a * b << endl;
+    cout << "Pembagian   : " << a / b << endl;
+    cout << "Sisa        : " << a % b << endl;
+
+    return 0;
+}
