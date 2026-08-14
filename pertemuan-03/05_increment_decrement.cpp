6c8e8fe556deb1d47256783505b4925cdfb1297c
illhammf <illhammf@users.noreply.github.com>
2026-08-15 04:38:16 +0700

    update 05_increment_decrement.cpp

---
+++ pertemuan-03/05_increment_decrement.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int angka = 10;
+
+    cout << "Nilai awal : " << angka << endl;
+
+    angka++;
+    cout << "Setelah ++ : " << angka << endl;
+
+    angka++;
+    cout << "Setelah ++ : " << angka << endl;
+
+    angka--;
+    cout << "Setelah -- : " << angka << endl;
+
+    angka--;
+    cout << "Setelah -- : " << angka << endl;
+
+    return 0;
+}
