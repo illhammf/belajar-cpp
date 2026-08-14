4e1fb67f6e63e8bff4500124c1b1597e7ebc34b4
illhammf <illhammf@users.noreply.github.com>
2026-08-15 04:39:20 +0700

    update 05_increment_decrement.cpp

--- pertemuan-03/05_increment_decrement.cpp
+++ pertemuan-03/05_increment_decrement.cpp
@@ -8,13 +8,13 @@ int main()
     cout << "Nilai awal : " << angka << endl;
 
     angka++;
-    cout << "Setelah ++ : " << angka << endl;
+    cout << "Setelah ++ : " << angka << endl; // Operator increment (++) akan menambahkan nilai variabel sebesar 1
 
     angka++;
     cout << "Setelah ++ : " << angka << endl;
 
     angka--;
-    cout << "Setelah -- : " << angka << endl;
+    cout << "Setelah -- : " << angka << endl; // Operator decrement (--) akan mengurangi nilai variabel sebesar 1
 
     angka--;
     cout << "Setelah -- : " << angka << endl;
