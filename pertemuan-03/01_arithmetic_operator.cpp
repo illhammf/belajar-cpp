9b29946a925f4a200c041f8f2d10a027664c2c71
illhammf <illhammf@users.noreply.github.com>
2026-08-14 18:41:45 +0700

    update 01_arithmetic_operator.cpp

---
+++ pertemuan-03/01_arithmetic_operator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    int b = 3;
+
+    cout << "=== ARITHMETIC OPERATOR ===" << endl;
+
+    cout << "Penjumlahan : " << a + b << endl;
+    cout << "Pengurangan : " << a - b << endl;
+    cout << "Perkalian   : " << a * b << endl;
+    cout << "Pembagian   : " << a / b << endl;
+    cout << "Sisa        : " << a % b << endl;
+
+    return 0;
+}
