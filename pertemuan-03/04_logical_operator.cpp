ce8e170c62cb86cdd08c207b51833fad09f17ad9
illhammf <illhammf@users.noreply.github.com>
2026-08-15 04:36:45 +0700

    update 04_logical_operator.cpp

--- pertemuan-03/04_logical_operator.cpp
+++ pertemuan-03/04_logical_operator.cpp
@@ -8,12 +8,15 @@ int main()
     bool kondisi1 = umur >= 17;
     bool kondisi2 = umur <= 30;
 
+    cout << "=== Cek Kondisi ===" << endl;
     cout << "Kondisi 1 : " << kondisi1 << endl;
     cout << "Kondisi 2 : " << kondisi2 << endl;
-
-    cout << "AND : " << (kondisi1 && kondisi2) << endl;
-    cout << "OR  : " << (kondisi1 || kondisi2) << endl;
-    cout << "NOT : " << (!kondisi1) << endl;
+    
+    cout << "=== LOGICAL OPERATOR ===" << endl;
+    // Operator logika digunakan untuk menggabungkan beberapa kondisi
+    cout << "AND : " << (kondisi1 && kondisi2) << endl; // Operator AND (&&) akan menghasilkan true jika kedua kondisi bernilai true
+    cout << "OR  : " << (kondisi1 || kondisi2) << endl; // Operator OR (||) akan menghasilkan true jika salah satu kondisi bernilai true
+    cout << "NOT : " << (!kondisi1) << endl; // Operator NOT (!) akan menghasilkan nilai kebalikan dari kondisi, jika kondisi bernilai true maka hasilnya false, dan sebaliknya
 
     return 0;
 }
