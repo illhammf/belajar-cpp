b5cc53216066fb9eed8b99e768057e37add58563
illhammf <illhammf@users.noreply.github.com>
2026-08-15 05:13:44 +0700

    update 07_expression.cpp

---
+++ pertemuan-03/07_expression.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    int b = 5;
+
+    int hasil1 = a + b;
+    int hasil2 = (a + b) * 2;
+    int hasil3 = a * b + 10;
+
+    cout << "Hasil 1 : " << hasil1 << endl;
+    cout << "Hasil 2 : " << hasil2 << endl;
+    cout << "Hasil 3 : " << hasil3 << endl;
+
+    return 0;
+}
