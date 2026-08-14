e23e90a462d9553635245b4711cbfa9b7ed7b759
illhammf <illhammf@users.noreply.github.com>
2026-08-15 04:31:53 +0700

    update 03_comparison_operator.cpp

--- pertemuan-03/03_comparison_operator.cpp
+++ pertemuan-03/03_comparison_operator.cpp
@@ -15,5 +15,8 @@ int main()
     cout << "a == b : " << (a == b) << endl;
     cout << "a != b : " << (a != b) << endl;
 
+    // Hasil dari operator perbandingan akan menghasilkan nilai boolean (true atau false)
+    // Dalam C++, true direpresentasikan sebagai 1 dan false direpresentasikan sebagai 0
+
     return 0;
 }
