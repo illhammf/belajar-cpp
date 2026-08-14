f9cfca7bfbfa8a77f802e57576acef05630f2538
illhammf <illhammf@users.noreply.github.com>
2026-08-15 05:12:33 +0700

    update 06_prefix_postfix.cpp

--- pertemuan-03/06_prefix_postfix.cpp
+++ pertemuan-03/06_prefix_postfix.cpp
@@ -6,15 +6,20 @@ int main()
     int angka1 = 10;
     int angka2 = 10;
 
+    // Operator increment (++) dan decrement (--) dapat digunakan dalam dua bentuk, yaitu prefix dan postfix
+    // Prefix: Operator increment/decrement ditempatkan sebelum variabel, sehingga nilai variabel akan diubah terlebih dahulu sebelum digunakan dalam ekspresi
+    // Postfix: Operator increment/decrement ditempatkan setelah variabel, sehingga nilai variabel akan digunakan terlebih dahulu dalam ekspresi sebelum diubah
     int hasil1 = angka1++;
     int hasil2 = ++angka2;
 
+    // Menampilkan hasil
     cout << "=== POSTFIX ===" << endl;
     cout << "Hasil : " << hasil1 << endl;
     cout << "Angka : " << angka1 << endl;
 
-    cout << endl;
+    cout << endl; // 
 
+    // Menampilkan hasil
     cout << "=== PREFIX ===" << endl;
     cout << "Hasil : " << hasil2 << endl;
     cout << "Angka : " << angka2 << endl;
