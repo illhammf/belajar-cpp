685dc3f7cf598583b888819c12e4e846a71650b7
illhammf <illhammf@users.noreply.github.com>
2026-08-16 07:34:31 +0700

    update 01_if.cpp

---
+++ pertemuan-04/01_if.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int umur = 20;
+
+    if (umur >= 17) // Jika umur lebih besar atau sama dengan 17
+    {
+        cout << "Kamu sudah cukup umur." << endl;
+    }
+
+    return 0;
+}
