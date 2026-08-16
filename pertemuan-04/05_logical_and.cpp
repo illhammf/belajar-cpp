25625fb24969d9a8a326c65f4dff2ec4c7ab384e
illhammf <illhammf@users.noreply.github.com>
2026-08-16 08:50:19 +0700

    update 05_logical_and.cpp

--- pertemuan-04/05_logical_and.cpp
+++ pertemuan-04/05_logical_and.cpp
@@ -8,6 +8,8 @@ int main()
     cout << "Masukkan umur: ";
     cin >> umur;
 
+    // Menggunakan operator logika AND (&&) untuk memeriksa apakah umur berada dalam rentang tertentu
+    // Bisa disebut DAN (AND) karena kita memeriksa dua kondisi sekaligus: umur harus lebih besar atau sama dengan 17 DAN kurang dari atau sama dengan 30.
     if (umur >= 17 && umur <= 30) // Jika umur berada dalam rentang 17 sampai 30
     {
         cout << "Umur berada dalam rentang 17 sampai 30." << endl;
