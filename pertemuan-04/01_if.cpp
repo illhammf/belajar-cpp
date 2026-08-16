c41079d3c8ae869013cd35c5469825ede03f7edc
illhammf <illhammf@users.noreply.github.com>
2026-08-16 07:37:10 +0700

    add 02_if_else.cpp

--- pertemuan-04/01_if.cpp
+++ pertemuan-04/01_if.cpp
@@ -10,6 +10,7 @@ int main()
         cout << "Kamu sudah cukup umur." << endl;
     }
     // Kalo kita membuat umur kurang dari 17, maka tidak akan menampilkan output apapun karena tidak ada kondisi else yang menangani kasus tersebut.
+    // Kita lanjut ke file berikutnya untuk menambahkan kondisi else agar program bisa menangani kasus umur kurang dari 17.
 
     return 0;
 }
