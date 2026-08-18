e65532572edcbcee24f8b3b82cdec00999d0601a
illhammf <illhammf@users.noreply.github.com>
2026-08-18 11:08:42 +0700

    update 05_for.cpp

---
+++ pertemuan-05/05_for.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Perulangan for digunakan untuk melakukan perulangan dengan jumlah yang telah ditentukan
+    for (int i = 1; i <= 5; i++)
+    {
+        cout << i << endl;
+    }
+
+    return 0;
+}
