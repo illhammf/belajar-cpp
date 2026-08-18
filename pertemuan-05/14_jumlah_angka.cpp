d5d803a4df44aa0ef4e6b90b6759bba947f76705
illhammf <illhammf@users.noreply.github.com>
2026-08-18 11:57:07 +0700

    update 14_jumlah_angka.cpp

---
+++ pertemuan-05/14_jumlah_angka.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int total = 0;
+
+    for (int i = 1; i <= 5; i++)
+    {
+        total += i;
+    }
+
+    cout << "Total: " << total << endl;
+
+    return 0;
+}
