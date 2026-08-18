7e9416bd7015662265c9165df670c4bf59e32dd3
illhammf <illhammf@users.noreply.github.com>
2026-08-18 11:14:19 +0700

    update 08_nested_loop.cpp

--- pertemuan-05/08_nested_loop.cpp
+++ pertemuan-05/08_nested_loop.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main()
 {
+    // Nested loop digunakan untuk melakukan perulangan di dalam perulangan
     for (int i = 1; i <= 3; i++)
     {
         for (int j = 1; j <= 3; j++)
