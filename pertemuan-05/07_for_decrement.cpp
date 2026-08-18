cca204ffd6101854558f2e0f1f9773dbfe327ac1
illhammf <illhammf@users.noreply.github.com>
2026-08-18 11:13:02 +0700

    update 07_for_decrement.cpp

---
+++ pertemuan-05/07_for_decrement.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Perulangan for digunakan untuk melakukan perulangan dengan jumlah yang telah ditentukan
+    // Memakai decrement, perulangan akan dilakukan dari angka 5 hingga 1
+    for (int i = 5; i >= 1; i--)
+    {
+        cout << i << endl;
+    }
+
+    return 0;
+}
