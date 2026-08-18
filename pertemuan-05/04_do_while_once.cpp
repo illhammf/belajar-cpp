760f70507af79b412d63d91662cc109ccb795011
illhammf <illhammf@users.noreply.github.com>
2026-08-18 11:05:17 +0700

    update 04_do_while_once.cpp

---
+++ pertemuan-05/04_do_while_once.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int angka = 10;
+
+    // Jalankan terlebih dahulu, kemudian cek kondisi
+    do
+    {
+        cout << "Kode dijalankan." << endl;
+    }
+    while (angka < 5);
+
+    return 0;
+}
