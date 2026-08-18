25a0b35f157b270f9f8b565c4481689ce33af652
illhammf <illhammf@users.noreply.github.com>
2026-08-18 12:03:22 +0700

    update 16_countdown.cpp

---
+++ pertemuan-05/16_countdown.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 10; i >= 1; i--) // Perulangan for digunakan untuk melakukan perulangan dengan jumlah yang telah ditentukan
+    {
+        cout << i << endl;
+    }
+
+    cout << "Selesai!" << endl;
+
+    return 0;
+}
