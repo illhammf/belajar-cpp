7d72386d9a19c72e3d7a48ffc2e8238d128436ff
illhammf <illhammf@users.noreply.github.com>
2026-08-18 10:52:16 +0700

    update 01_while.cpp

---
+++ pertemuan-05/01_while.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int angka = 1;
+
+    while (angka <= 5)
+    {
+        cout << angka << endl;
+
+        angka++;
+    }
+
+    return 0;
+}
