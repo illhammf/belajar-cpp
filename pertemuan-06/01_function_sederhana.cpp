05e94586654d85a900aed4136f928d1925647080
illhammf <illhammf@users.noreply.github.com>
2026-08-19 15:18:15 +0700

    update 01_function_sederhana.cpp

---
+++ pertemuan-06/01_function_sederhana.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void sapa() // fungsi sederhana untuk menyapa
+{
+    cout << "Halo, selamat belajar C++!" << endl;
+}
+
+int main()
+{
+    sapa(); // memanggil fungsi sapa
+
+    return 0;
+}
