7f7bba8307d073e8ec278f7239d72c9a6f6b8ce7
illhammf <illhammf@users.noreply.github.com>
2026-08-20 11:14:36 +0700

    add 11_function_jumlah.cpp

---
+++ pertemuan-06/11_function_jumlah.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int jumlahAngka(int batas)
+{
+    int total = 0;
+
+    for (int i = 1; i <= batas; i++)
+    {
+        total += i;
+    }
+
+    return total;
+}
+
+int main()
+{
+    int hasil = jumlahAngka(5);
+
+    cout << "Total: " << hasil << endl;
+
+    return 0;
+}
