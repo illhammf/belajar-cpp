11befaa87338a17e87ffb7b5f7e63889e471a7db
illhammf <illhammf@users.noreply.github.com>
2026-08-20 10:37:00 +0700

    update 03_function_parameter.cpp

---
+++ pertemuan-06/03_function_parameter.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void sapa(string nama)
+{
+    cout << "Halo, " << nama << "!" << endl;
+}
+
+int main()
+{
+    sapa("Ilham");
+    sapa("Budi");
+    sapa("Andi");
+
+    return 0;
+}
