a8fd880405c2a3a3f20c40f76c13b473eeb0d948
illhammf <illhammf@users.noreply.github.com>
2026-08-20 10:55:11 +0700

    update 07_function_boolean.cpp

---
+++ pertemuan-06/07_function_boolean.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+bool isGenap(int angka) // Fungsi untuk memeriksa apakah sebuah bilangan genap atau tidak
+{
+    return angka % 2 == 0; // Dibaca sebagai "kembalikan true jika angka habis dibagi 2, jika tidak kembalikan false"
+}
+
+int main()
+{
+    cout << isGenap(10) << endl;
+    cout << isGenap(7) << endl;
+
+    return 0;
+}
