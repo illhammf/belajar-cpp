14bdb64f5a586c3a78b4cdbce3d3fe185b5573b3
illhammf <illhammf@users.noreply.github.com>
2026-08-20 11:07:09 +0700

    update 09_function_dengan_loop.cpp

---
+++ pertemuan-06/09_function_dengan_loop.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void tampilkanAngka()
+{
+    for (int i = 1; i <= 5; i++)
+    {
+        cout << i << endl;
+    }
+}
+
+int main()
+{
+    tampilkanAngka();
+
+    return 0;
+}
