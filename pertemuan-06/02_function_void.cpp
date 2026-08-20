97b2fbf86df31585dc9a798e4333e4a035f78af9
illhammf <illhammf@users.noreply.github.com>
2026-08-20 10:32:37 +0700

    update 02_function_void.cpp

---
+++ pertemuan-06/02_function_void.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void tampilkanNama()
+{
+    cout << "Nama: Ilham" << endl;
+}
+
+void tampilkanJurusan()
+{
+    cout << "Jurusan: teknik Informatika" << endl;
+}
+
+void tampilkanUniversitas()
+{
+    cout << "Universitas: Universitas Esa Unggul" << endl;
+}
+
+int main()
+{
+    tampilkanNama();
+    tampilkanJurusan();
+    tampilkanUniversitas();
+
+    return 0;
+}
