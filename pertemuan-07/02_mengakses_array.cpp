1c29e0a5a8df717dd65f70e6ba40ae79dcdb6f50
illhammf <illhammf@users.noreply.github.com>
2026-08-20 11:49:44 +0700

    update 02_mengakses_array.cpp

---
+++ pertemuan-07/02_mengakses_array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int nilai[5] = {80, 90, 75, 88, 92};
+
+    cout << "Nilai pertama  : " << nilai[0] << endl;
+    cout << "Nilai kedua    : " << nilai[1] << endl;
+    cout << "Nilai ketiga   : " << nilai[2] << endl;
+    cout << "Nilai keempat  : " << nilai[3] << endl;
+    cout << "Nilai kelima   : " << nilai[4] << endl;
+
+    return 0;
+}
