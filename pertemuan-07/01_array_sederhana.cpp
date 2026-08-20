c841c671f8a5023b8386d8950052e54eaa7204f5
illhammf <illhammf@users.noreply.github.com>
2026-08-20 11:48:41 +0700

    update 01_array_sederhana.cpp

---
+++ pertemuan-07/01_array_sederhana.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Membuat array sederhana dengan tipe data integer dan ukuran 5
+    int angka[5] = {10, 20, 30, 40, 50};
+
+    cout << angka[0] << endl;
+    cout << angka[1] << endl;
+    cout << angka[2] << endl;
+    cout << angka[3] << endl;
+    cout << angka[4] << endl;
+
+    return 0;
+}
