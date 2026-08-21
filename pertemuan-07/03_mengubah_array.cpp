f4dc8165db6f8f96d35469a62eeaede7f10e5931
illhammf <illhammf@users.noreply.github.com>
2026-08-21 17:23:17 +0700

    update 03_mengubah_array.cpp

---
+++ pertemuan-07/03_mengubah_array.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int nilai[5] = {80, 90, 75, 88, 92};
+
+    cout << "Sebelum diubah:" << endl;
+
+    cout << nilai[0] << endl;
+    cout << nilai[1] << endl;
+    cout << nilai[2] << endl;
+    cout << nilai[3] << endl;
+    cout << nilai[4] << endl;
+
+    nilai[0] = 100;
+
+    cout << endl;
+
+    cout << "Setelah diubah:" << endl;
+
+    cout << nilai[0] << endl;
+    cout << nilai[1] << endl;
+    cout << nilai[2] << endl;
+    cout << nilai[3] << endl;
+    cout << nilai[4] << endl;
+
+    return 0;
+}
