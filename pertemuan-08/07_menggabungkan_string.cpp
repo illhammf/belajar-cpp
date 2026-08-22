b99d2d20ed37a400c154425ed11a24db969a2f96
illhammf <illhammf@users.noreply.github.com>
2026-08-22 14:34:59 +0700

    update 07_menggabungkan_string.cpp

---
+++ pertemuan-08/07_menggabungkan_string.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string namaDepan = "Ilham";
+    string namaBelakang = "Firmansyah";
+
+    string namaLengkap = namaDepan + " " + namaBelakang;
+
+    cout << "Nama lengkap: " << namaLengkap << endl;
+
+    return 0;
+}
