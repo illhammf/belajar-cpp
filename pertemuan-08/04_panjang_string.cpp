664c89a3e8a2e00f64905955de099c5d34f26ed4
illhammf <illhammf@users.noreply.github.com>
2026-08-22 14:20:04 +0700

    update 04_panjang_string.cpp

---
+++ pertemuan-08/04_panjang_string.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string nama = "Ilham";
+
+    cout << "Nama: " << nama << endl;
+    cout << "Panjang: " << nama.length() << endl; // length() digunakan untuk menghitung panjang string
+
+    return 0;
+}
