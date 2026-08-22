7a238f59a82aa34eadadd0057264e59bcaae9b5a
illhammf <illhammf@users.noreply.github.com>
2026-08-22 14:05:19 +0700

    update 01_string_sederhana.cpp

---
+++ pertemuan-08/01_string_sederhana.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string nama = "Ilham"; // Deklarasi dan inisialisasi variabel string
+
+    cout << "Nama: " << nama << endl;
+
+    return 0;
+}
