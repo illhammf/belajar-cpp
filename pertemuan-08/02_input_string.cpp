6a9d374d8d0d8af1a8cc845684d8c7d102ccb89e
illhammf <illhammf@users.noreply.github.com>
2026-08-22 14:11:54 +0700

    UPDATE 02_input_string.cpp

---
+++ pertemuan-08/02_input_string.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string> // Library untuk penggunaan tipe data string
+
+using namespace std;
+
+int main()
+{
+    string nama; // 
+
+    cout << "Masukkan nama: ";
+    cin >> nama;
+
+    cout << "Halo, " << nama << endl;
+
+    return 0;
+}
