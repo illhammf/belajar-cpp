85216a3c7cb178cb54313af1e3404b4236ad37b5
illhammf <illhammf@users.noreply.github.com>
2026-08-22 14:22:41 +0700

    update 05_mengakses_karakter.cpp

---
+++ pertemuan-08/05_mengakses_karakter.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string nama = "Ilham";
+
+    cout << "Karakter pertama: " << nama[0] << endl;
+    cout << "Karakter kedua  : " << nama[1] << endl;
+    cout << "Karakter ketiga : " << nama[2] << endl;
+
+    return 0;
+}
