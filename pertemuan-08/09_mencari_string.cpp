fd411854e8d88a45b5da5ee764eb0230e8600863
illhammf <illhammf@users.noreply.github.com>
2026-08-22 21:47:29 +0700

    update 09_mencari_string.cpp

--- pertemuan-08/09_mencari_string.cpp
+++ pertemuan-08/09_mencari_string.cpp
@@ -7,7 +7,7 @@ int main()
 {
     string kalimat = "Saya sedang belajar C++";
 
-    int posisi = kalimat.find("C++");
+    int posisi = kalimat.find("C++"); // find() digunakan untuk mencari posisi string
 
     cout << "Posisi C++: " << posisi << endl;
 
