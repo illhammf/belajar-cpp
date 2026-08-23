8a722c7f63103d6e743cbaa21bbadc361c328955
illhammf <illhammf@users.noreply.github.com>
2026-08-23 22:22:43 +0700

    update 01_alamat_memory.cpp

---
+++ pertemuan-09/01_alamat_memory.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int angka = 10;
+
+    cout << "Nilai angka  : " << angka << endl;
+    cout << "Alamat angka : " << &angka << endl; // & digunakan untuk mendapatkan alamat dari variabel
+
+    return 0;
+}
