eec7dee8ece8b0c5fec91a4c3a2d305c8277fa71
illhammf <illhammf@users.noreply.github.com>
2026-08-24 08:15:13 +0700

    update 02_pointer_sederhana.cpp

--- pertemuan-09/02_pointer_sederhana.cpp
+++ pertemuan-09/02_pointer_sederhana.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main()
 {
-    int angka = 10;
+    int angka = 10; // variabel biasa
 
-    int* pointer = &angka;
+    int* pointer = &angka; // pointer yang menunjuk ke variabel angka, int* digunakan untuk mendeklarasikan pointer yang menunjuk ke tipe data int
 
     cout << "Nilai angka : " << angka << endl;
     cout << "Isi pointer : " << pointer << endl;
