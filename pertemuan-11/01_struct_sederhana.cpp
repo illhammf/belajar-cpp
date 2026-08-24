039b9ae5fc7d457a98fe6283d9864c2733749d38
illhammf <illhammf@users.noreply.github.com>
2026-08-24 15:01:03 +0700

    update 01_struct_sederhana.cpp

--- pertemuan-11/01_struct_sederhana.cpp
+++ pertemuan-11/01_struct_sederhana.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+// Struct mahasiswa yang isinya ada variabel nama, umur, ipk
 struct Mahasiswa
 {
     string nama;
@@ -14,9 +15,9 @@ int main()
 {
     Mahasiswa mahasiswa;
 
-    mahasiswa.nama = "Ilham";
-    mahasiswa.umur = 20;
-    mahasiswa.ipk = 3.50;
+    mahasiswa.nama = "Ilham"; // isi dari variabel nama
+    mahasiswa.umur = 20; // isi dari variabel umur
+    mahasiswa.ipk = 3.50; // isi dari variabel ipk
 
     cout << "Nama: " << mahasiswa.nama << endl;
     cout << "Umur: " << mahasiswa.umur << endl;
