13c49f54aa51cdd73b44ecb3bc5b7bf3f62fdd63
illhammf <illhammf@users.noreply.github.com>
2026-08-24 08:40:57 +0700

    update 10_nullptr.cpp

---
+++ pertemuan-09/10_nullptr.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int* pointer = nullptr;
+
+    if (pointer == nullptr)
+    {
+        cout << "Pointer belum menunjuk ke data." << endl;
+    }
+
+    return 0;
+}
