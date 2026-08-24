c44c1db53f5c3f1fd6922f10dc292ec4d59a602f
illhammf <illhammf@users.noreply.github.com>
2026-08-24 08:16:59 +0700

    update 03_operator_dereference.cpp

---
+++ pertemuan-09/03_operator_dereference.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int angka = 10;
+
+    int* pointer = &angka;
+
+    cout << "Nilai angka        : " << angka << endl;
+    cout << "Alamat angka       : " << &angka << endl;
+    cout << "Isi pointer        : " << pointer << endl;
+    cout << "Nilai dari pointer : " << *pointer << endl;
+
+    return 0;
+}
