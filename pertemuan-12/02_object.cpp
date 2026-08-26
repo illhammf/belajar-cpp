4856758f87f977677c6e134afe74f819b889e5d3
illhammf <illhammf@users.noreply.github.com>
2026-08-26 19:49:52 +0700

    update 02_object.cpp

---
+++ pertemuan-12/02_object.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Mahasiswa
+{
+public:
+    string nama;
+    int umur;
+};
+
+int main()
+{
+    Mahasiswa mahasiswa1;
+    Mahasiswa mahasiswa2;
+
+    mahasiswa1.nama = "Ilham";
+    mahasiswa1.umur = 20;
+
+    mahasiswa2.nama = "Budi";
+    mahasiswa2.umur = 21;
+
+    cout << "Mahasiswa 1" << endl;
+    cout << "Nama: " << mahasiswa1.nama << endl;
+    cout << "Umur: " << mahasiswa1.umur << endl;
+
+    cout << endl;
+
+    cout << "Mahasiswa 2" << endl;
+    cout << "Nama: " << mahasiswa2.nama << endl;
+    cout << "Umur: " << mahasiswa2.umur << endl;
+
+    return 0;
+}
