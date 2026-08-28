84263f2f6724dcecfe56b6c8a387c0891c707e05
illhammf <illhammf@users.noreply.github.com>
2026-08-28 13:21:55 +0700

    update 12_challenge_class.cpp

---
+++ pertemuan-12/12_challenge_class.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Buku
+{
+private:
+    string judul;
+    string penulis;
+    int tahun;
+
+public:
+
+    Buku(string judul, string penulis, int tahun)
+    {
+        this->judul = judul;
+        this->penulis = penulis;
+        this->tahun = tahun;
+    }
+
+    void tampilkanData()
+    {
+        cout << "=== Data Buku ===" << endl;
+        cout << "Judul   : " << judul << endl;
+        cout << "Penulis : " << penulis << endl;
+        cout << "Tahun   : " << tahun << endl;
+    }
+
+    void setJudul(string judul)
+    {
+        this->judul = judul;
+    }
+
+    void setPenulis(string penulis)
+    {
+        this->penulis = penulis;
+    }
+
+    void setTahun(int tahun)
+    {
+        this->tahun = tahun;
+    }
+
+    string getJudul()
+    {
+        return judul;
+    }
+
+    string getPenulis()
+    {
+        return penulis;
+    }
+
+    int getTahun()
+    {
+        return tahun;
+    }
+};
+
+int main()
+{
+    Buku buku(
+        "Belajar C++",
+        "Ilham",
+        2026
+    );
+
+    buku.tampilkanData();
+
+    return 0;
+}
